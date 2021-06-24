aa94cea9ba5e8cecbd1be21bf1dfab15f965ebc4
Mickael FOSSO <fosso.mickael@gmail.com>
2021-06-24 20:01:57 +0200

    clean fake data

--- src/actions/cars.js
+++ src/actions/cars.js
@@ -1,10 +1,5 @@
 const BASE_URL = 'https://wagon-garage-api.herokuapp.com';
-export const carsData = [
-    { id: 1, brand: 'Peugeot', model: '106', owner: 'John', plate: 'WOB-ED-42' },
-    { id: 2, brand: 'Renault', model: 'Scenic', owner: 'Paul', plate: 'AAA-12-BC' },
-    { id: 3, brand: 'Aston Martin', model: 'DB Mark III', owner: 'James', plate: '418-ED-94' },
-    { id: 4, brand: 'VW', model: 'Beetle', owner: 'George', plate: '1234-XD-75' }
-  ];
+
 
 export const setCars = (garage) => { 
     return {
